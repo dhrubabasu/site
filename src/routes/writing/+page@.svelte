3ea65fe21f4a412825b35124c43aa831c5ef5f6b
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2024-08-05 00:50:57 -0400

    class nit

--- src/routes/writing/+page@.svelte
+++ src/routes/writing/+page@.svelte
@@ -17,7 +17,7 @@
 
 	{#each writings as { href, date, title }, idx}
 		<div class:mt-4={idx > 0}>
-			<span class="text-sm leading-7 text-gray-700">{date}</span>
+			<span class="text-sm font-normal text-gray-700">{date}</span>
 			<br />
 			<a class="text-lg" {href}>{title}</a>
 		</div>
