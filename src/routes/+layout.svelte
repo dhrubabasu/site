d36896220a79a5a5d988e2790935ee88b0dc6bc5
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2024-08-06 20:19:16 -0400

    remove components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,45 @@
 <script>
-	import Header from '$lib/components/Header.svelte';
 	import '../app.css';
+	import { page } from '$app/stores';
+
+	const links = [
+		{
+			href: '/',
+			name: 'Home',
+		},
+		{
+			href: '/now',
+			name: 'Now',
+		},
+		{
+			href: '/about',
+			name: 'About',
+		},
+		{
+			href: '/writing',
+			name: 'Writing',
+		},
+		{
+			href: '/mentions',
+			name: 'Mentions',
+		},
+	];
 </script>
 
 <svelte:head>
 	<title>Dhruba Basu</title>
 </svelte:head>
 
-<Header />
+<div class="my-8 mx-auto prose">
+	<code class="text-lg font-bold after:content-none before:content-none">/dhrubabasu</code>
+	<div class="flow-root mt-4">
+		<div class="flex flex-wrap items-center -my-2 -mx-1">
+			{#each links as { href, name }, idx}
+				<a class="mx-1 mt-2 mb-1" class:no-underline={$page.url.pathname == href} {href}>{name}</a>
+				{#if idx != links.length - 1}<span class="mt-2 mb-1">•</span>{/if}
+			{/each}
+		</div>
+	</div>
+</div>
+
 <slot />
