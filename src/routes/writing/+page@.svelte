b47d3e13d3c9db6d5922659a403ab0273460023a
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2024-12-31 03:19:35 -0500

    new `/writing` page

--- src/routes/writing/+page@.svelte
+++ src/routes/writing/+page@.svelte
@@ -2,7 +2,7 @@
 	const writings = [
 		{
 			href: '/writing/2024/01/02/mev-vs-pfof',
-			date: 'January 2, 2024',
+			date: new Date(Date.UTC(2024, 0, 2, 8)),
 			title: 'MEV vs PFOF',
 		},
 	];
@@ -15,11 +15,16 @@
 <div class="mx-auto prose dark:prose-invert">
 	<h2>Writing</h2>
 
-	{#each writings as { href, date, title }, idx}
-		<div class:mt-4={idx > 0}>
-			<span class="text-sm font-normal text-zinc-700 dark:text-zinc-200">{date}</span>
-			<br />
-			<a class="text-lg" {href}>{title}</a>
-		</div>
-	{/each}
+	<div>
+		{#each writings as { href, title, date }}
+			<div
+				class="flex flex-col justify-between border-zinc-300 py-3 md:flex-row md:items-center md:py-2 dark:border-zinc-700 [&:first-child]:pt-0 [&:not(:last-child)]:border-b"
+			>
+				<a class="text-lg font-semibold" {href}>{title}</a>
+				<div class="text-sm tracking-wide font-left min-w-32 text-zinc-600 dark:text-zinc-400">
+					{date.toLocaleDateString(undefined, { year: 'numeric', month: 'long', day: 'numeric' })}
+				</div>
+			</div>
+		{/each}
+	</div>
 </div>
