cdf102dd9e5910a1c474dd3d665e361abed179e1
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2025-01-04 09:11:06 -0500

    use split bundle strategy

--- svelte.config.js
+++ svelte.config.js
@@ -9,9 +9,6 @@ const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter(),
-		output: {
-			bundleStrategy: 'single',
-		},
 		prerender: {
 			origin: 'https://dhruba.xyz',
 		},
