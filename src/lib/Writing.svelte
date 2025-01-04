d41421395580cd3d5aef3d31da5f28c65cfbf3aa
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2025-01-04 01:56:14 -0500

    extract writing layout to a `Writing` component

---
+++ src/lib/Writing.svelte
@@ -0,0 +1,126 @@
+<script lang="ts">
+	let selectors = 'h3, h4';
+	let headings: Element[] = $state([]);
+	let bolded = $state('');
+	let open = $state(false);
+
+	function onScroll() {
+		const numHeadings = headings.length;
+		if (numHeadings == 0) return;
+
+		const { scrollY, innerHeight } = window;
+		if (scrollY <= 0) {
+			bolded = headings[0].id;
+			return;
+		}
+
+		if (scrollY + innerHeight >= document.body.scrollHeight) {
+			bolded = headings[numHeadings - 1].id;
+			return;
+		}
+
+		const middle = innerHeight * 0.4;
+		let current = headings[0].id;
+		for (let i = 0; i < numHeadings; i++) {
+			if (middle >= headings[i].getBoundingClientRect().top) {
+				current = headings[i].id;
+			}
+		}
+		bolded = current;
+	}
+
+	import { onMount } from 'svelte';
+	onMount(() => {
+		const { hash } = window.location;
+		if (hash) {
+			const id = hash.substring(1);
+			const el = document.getElementById(id);
+			if (el) {
+				el.scrollIntoView();
+				bolded = id;
+			}
+		}
+
+		headings = Array.from(document.querySelectorAll(selectors)).filter(({ id }) => id.length > 0);
+
+		onScroll();
+	});
+
+	import type { Snippet } from 'svelte';
+	let { title, readingTime, date, children }: { title: string; readingTime: number; date: Date; children: Snippet } =
+		$props();
+
+	import { debounce } from '$lib/debounce';
+</script>
+
+<svelte:window on:scroll={debounce(onScroll)} on:load={debounce(onScroll)} />
+
+<svelte:head>
+	<title>{title} - Dhruba Basu</title>
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:title" content={title} />
+</svelte:head>
+
+{#snippet toc(headings: Element[])}
+	<span class="mb-3 text-xs font-semibold uppercase tracking-wide text-zinc-900 dark:text-zinc-100"> contents </span>
+	{#each headings as { id, nodeName, textContent }}
+		<p class:ml-4={nodeName == selectors.slice(-2).toUpperCase()}>
+			<a
+				onclick={() => (open = false)}
+				href="#{id}"
+				class="m-0 block transform border-l-2 border-transparent p-0 pl-2 text-sm no-underline transition-colors {id ==
+				bolded
+					? 'border-zinc-800 dark:border-zinc-200'
+					: ''}">{textContent}</a
+			>
+		</p>
+	{/each}
+{/snippet}
+
+{#if headings.length > 0}
+	<div class="fixed inset-y-0 right-0 hidden max-w-56 flex-none pr-8 xl:block">
+		<div class="top-4 flex max-h-screen flex-col justify-between overflow-y-auto pb-6 pt-10">
+			<div class="mb-8 space-y-4">
+				{@render toc(headings)}
+			</div>
+		</div>
+	</div>
+{/if}
+
+<h2 class="sticky top-0 z-10 -mt-4 border-b border-zinc-300 bg-zinc-100 py-4 dark:border-zinc-700 dark:bg-zinc-900">
+	<div class="flex items-center justify-between">
+		<span class="shrink">{title}</span>
+		<button
+			class="shrink-0 cursor-pointer rounded border border-zinc-300 p-1 hover:bg-zinc-200 xl:hidden dark:border-zinc-700 dark:hover:bg-zinc-800"
+			onclick={() => (open = !open)}
+			aria-label="Open table of contents."
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="size-6"
+			>
+				<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
+			</svg>
+		</button>
+	</div>
+
+	{#if open}
+		<div class="mt-1 space-y-2 pt-2 xl:hidden">
+			{@render toc(headings)}
+		</div>
+	{/if}
+</h2>
+
+<span class="text-sm font-normal tracking-wide text-zinc-700 dark:text-zinc-400"
+	>{readingTime} min read • {date.toLocaleDateString(undefined, {
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric',
+	})}</span
+>
+
+{@render children()}
--- src/routes/writing/2024/01/02/mev-vs-pfof/+page.svelte
+++ src/routes/writing/2024/01/02/mev-vs-pfof/+page.svelte
@@ -1,104 +1,98 @@
 <script>
 	import ExternalLink from '$lib/ExternalLink.svelte';
+	import Writing from '$lib/Writing.svelte';
 
 	import janMarketImg from './2024-01-02-coq-inu-markets.jpg';
 	import novMarketImg from './2024-11-27-coq-inu-markets.jpg';
 </script>
 
-<svelte:head>
-	<title>MEV vs PFOF - Dhruba Basu</title>
-</svelte:head>
+<Writing title="MEV vs PFOF" readingTime={3} date={new Date(Date.UTC(2024, 0, 2, 8))}>
+	<p>
+		<em
+			>Originally posted to <ExternalLink href="https://x.com/dhrubabasu_/status/1742242040820441196">X</ExternalLink>.
+			Minimally edited for website format.</em
+		>
+	</p>
 
-<h2 class="sticky top-0 -mt-4 border-b border-zinc-300 bg-zinc-100 py-4 dark:border-zinc-700 dark:bg-zinc-900">
-	MEV vs PFOF
-</h2>
+	<p>
+		Maximal Extractable Value (MEV) in crypto is often compared to or equated with Payment for Order Flow (PFOF) in
+		TradFi. However, each impacts the unsuspecting trader in very different ways. Why? Regulations.
+	</p>
 
-<span class="text-sm font-normal tracking-wide text-zinc-700 dark:text-zinc-400">3 min read • January 2, 2024</span>
-<p>
-	<em
-		>Originally posted to <ExternalLink href="https://x.com/dhrubabasu_/status/1742242040820441196">X</ExternalLink>.
-		Minimally edited for website format.</em
-	>
-</p>
+	<p>
+		In TradFi, the SEC and FINRA govern how PFOF can be used by trading firms and what they can or cannot do with the
+		order flow. FINRA member firms must not let PFOF interfere with their <ExternalLink
+			href="https://www.finra.org/rules-guidance/notices/21-23">duty of best execution</ExternalLink
+		>.
+	</p>
 
-<p>
-	Maximal Extractable Value (MEV) in crypto is often compared to or equated with Payment for Order Flow (PFOF) in
-	TradFi. However, each impacts the unsuspecting trader in very different ways. Why? Regulations.
-</p>
+	<p>
+		While reasonable people can disagree on whether or not these regulations are sufficiently effective, it is
+		indisputable that firms like Citadel or Jump Trading have to comply with these regulations.
+	</p>
 
-<p>
-	In TradFi, the SEC and FINRA govern how PFOF can be used by trading firms and what they can or cannot do with the
-	order flow. FINRA member firms must not let PFOF interfere with their <ExternalLink
-		href="https://www.finra.org/rules-guidance/notices/21-23">duty of best execution</ExternalLink
-	>.
-</p>
+	<p>
+		Traders using Robinhood and Schwab are, in theory, guaranteed that their order will be executed at the most
+		favorable terms reasonably available. Although both brokers profit from PFOF, SEC + FINRA <ExternalLink
+			href="https://www.sec.gov/files/34-96496-fact-sheet.pdf">certifies</ExternalLink
+		> best execution.
+	</p>
 
-<p>
-	While reasonable people can disagree on whether or not these regulations are sufficiently effective, it is
-	indisputable that firms like Citadel or Jump Trading have to comply with these regulations.
-</p>
+	<p>
+		In crypto, nobody registers with a FINRA-type org as it is a permissionless system. Block proposers have no duty of
+		best execution. This enables all kinds of bad behavior like MEV which extract value from unsuspecting traders.
+	</p>
 
-<p>
-	Traders using Robinhood and Schwab are, in theory, guaranteed that their order will be executed at the most favorable
-	terms reasonably available. Although both brokers profit from PFOF, SEC + FINRA <ExternalLink
-		href="https://www.sec.gov/files/34-96496-fact-sheet.pdf">certifies</ExternalLink
-	> best execution.
-</p>
+	<p>
+		jaredfromsubway.eth is perhaps one of the most famous MEV bots created in February 2023. This bot extracted upwards
+		of <ExternalLink href="https://www.theblock.co/post/230218/jaredfromsubway-mev-bot">$34 million</ExternalLink> in proceeds
+		from unsuspecting traders participating in on-chain trading.
+	</p>
 
-<p>
-	In crypto, nobody registers with a FINRA-type org as it is a permissionless system. Block proposers have no duty of
-	best execution. This enables all kinds of bad behavior like MEV which extract value from unsuspecting traders.
-</p>
+	<p>
+		With no enforcement of good behavior, jaredfromsubway.eth can extract as much as they please from unsuspecting
+		traders. In this way, MEV is <em>more</em> nefarious than PFOF. There is no recourse for victims of MEV, unlike for
+		<ExternalLink href="https://www.sec.gov/news/press-release/2020-321">PFOF</ExternalLink>.
+	</p>
 
-<p>
-	jaredfromsubway.eth is perhaps one of the most famous MEV bots created in February 2023. This bot extracted upwards of <ExternalLink
-		href="https://www.theblock.co/post/230218/jaredfromsubway-mev-bot">$34 million</ExternalLink
-	> in proceeds from unsuspecting traders participating in on-chain trading.
-</p>
+	<p>
+		This poses an existential threat to on-chain trading. Will the next billion users be willing to put up with this
+		hidden fee for the sake of being on-chain? Or will they opt to trade on venues that must comply with a regulatory
+		body, namely centralized exchanges like Coinbase?
+	</p>
 
-<p>
-	With no enforcement of good behavior, jaredfromsubway.eth can extract as much as they please from unsuspecting
-	traders. In this way, MEV is <em>more</em> nefarious than PFOF. There is no recourse for victims of MEV, unlike for
-	<ExternalLink href="https://www.sec.gov/news/press-release/2020-321">PFOF</ExternalLink>.
-</p>
+	<p>
+		Can we enforce good behavior cryptographically or is it necessary for a FINRA-type org to come in? There are
+		promising solutions like <ExternalLink href="https://penumbra.zone">Penumbra</ExternalLink> but a solution that does
+		not make UX sacrifices is elusive. Until then, centralized exchanges will continue to dominate.
+	</p>
 
-<p>
-	This poses an existential threat to on-chain trading. Will the next billion users be willing to put up with this
-	hidden fee for the sake of being on-chain? Or will they opt to trade on venues that must comply with a regulatory
-	body, namely centralized exchanges like Coinbase?
-</p>
+	<p>
+		CEX domination can even be shown through assets that originate on-chain. $COQ was minted and launched on the Trader
+		Joe DEX. KuCoin and others listed the asset following its success. Now, centralized exchanges are trading more $COQ
+		on aggregate than decentralized exchanges.
+	</p>
 
-<p>
-	Can we enforce good behavior cryptographically or is it necessary for a FINRA-type org to come in? There are promising
-	solutions like <ExternalLink href="https://penumbra.zone">Penumbra</ExternalLink> but a solution that does not make UX
-	sacrifices is elusive. Until then, centralized exchanges will continue to dominate.
-</p>
+	<figure>
+		<img src={janMarketImg} alt="List of top trading venues for $COQ on 2024/01/02" />
+		<figcaption class="text-center">On January 2 of 2024, Trader Joe was the top trading venue for $COQ.</figcaption>
+	</figure>
 
-<p>
-	CEX domination can even be shown through assets that originate on-chain. $COQ was minted and launched on the Trader
-	Joe DEX. KuCoin and others listed the asset following its success. Now, centralized exchanges are trading more $COQ on
-	aggregate than decentralized exchanges.
-</p>
+	<p>
+		Will decentralized exchanges be relegated to new asset launches? Once centralized exchanges list the asset, are
+		decentralized exchanges destined to lose market share?
+	</p>
 
-<figure>
-	<img src={janMarketImg} alt="List of top trading venues for $COQ on 2024/01/02" />
-	<figcaption class="text-center">On January 2 of 2024, Trader Joe was the top trading venue for $COQ.</figcaption>
-</figure>
+	<p>
+		<em>
+			Update (Nov 27, 2024): Trader Joe is no longer the top trading venue for $COQ. Bybit, the new top trading venue
+			for $COQ, saw an increase in market share from 8.96% to 20.29%. Trader Joe saw a decrease in market share from 31%
+			to 8%. Gate.io (16.15%) and BingX (12.14%) also lead Trader Joe in market share.
+		</em>
+	</p>
 
-<p>
-	Will decentralized exchanges be relegated to new asset launches? Once centralized exchanges list the asset, are
-	decentralized exchanges destined to lose market share?
-</p>
-
-<p>
-	<em>
-		Update (Nov 27, 2024): Trader Joe is no longer the top trading venue for $COQ. Bybit, the new top trading venue for
-		$COQ, saw an increase in market share from 8.96% to 20.29%. Trader Joe saw a decrease in market share from 31% to
-		8%. Gate.io (16.15%) and BingX (12.14%) also lead Trader Joe in market share.
-	</em>
-</p>
-
-<figure>
-	<img src={novMarketImg} alt="List of top trading venues for $COQ on 2024/11/27" />
-	<figcaption class="text-center">On November 27 of 2024, Bybit was the top trading venue for $COQ.</figcaption>
-</figure>
+	<figure>
+		<img src={novMarketImg} alt="List of top trading venues for $COQ on 2024/11/27" />
+		<figcaption class="text-center">On November 27 of 2024, Bybit was the top trading venue for $COQ.</figcaption>
+	</figure>
+</Writing>
