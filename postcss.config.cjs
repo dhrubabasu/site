61ae150d5f28e08adb4c1a2713f7df263dc8c85d
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2025-01-04 23:33:34 -0500

    disable for now

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+
 const dev = process.env.NODE_ENV === 'development';
 
 module.exports = {
--- src/lib/debounce.ts
+++ src/lib/debounce.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-this-alias, prefer-rest-params */
+
 export const debounce = <T extends (...args: any[]) => void>(func: T): (() => void) => {
 	let timeout: number;
 	return function (this: any) {
